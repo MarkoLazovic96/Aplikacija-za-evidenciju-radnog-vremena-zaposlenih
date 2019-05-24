4d5329bd14f3af5ea5ba8f8048dc16fb3509d4cc
MarkoLazovic15 <marko.lazovic.15@singimail.rs>
2019-05-24 23:43:43 +0200

    Twig installation, database update

--- configuration.php
+++ configuration.php
@@ -3,5 +3,5 @@ final class Configuration{
     const DATABASE_HOST = 'localhost';
     const DATABASE_USER = 'root';
     const DATABASE_PASS = '';
-    const DATABASE_NAME = 'evidencija';
+    const DATABASE_NAME = 'evrz';
 }
